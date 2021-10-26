4fe44daef6e6121738530ef5dae0473ed510c394
hgko1207 <khkkhk1207@gmail.com>
2021-10-27 00:14:29 +0900

    도메인 추가

---
+++ romas-web/src/main/java/net/woori/romas/domain/Domain.java
@@ -0,0 +1,11 @@
+package net.woori.romas.domain;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author hgko
+ *
+ */
+public interface Domain extends Serializable {
+}
---
+++ romas-web/src/main/java/net/woori/romas/domain/param/SearchParam.java
@@ -0,0 +1,14 @@
+package net.woori.romas.domain.param;
+
+import lombok.Data;
+
+/**
+ * 검색조건
+ * 
+ * @author hgko
+ *
+ */
+@Data
+public class SearchParam {
+
+}
